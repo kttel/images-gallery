0046e6ec983651588a9a5d1dd848f259c9f67699
elizabeth <63110747+kttel@users.noreply.github.com>
2023-02-22 10:03:53 +0200

    Added image card component with delete functionality

---
+++ frontend/src/components/ImageCard.js
@@ -0,0 +1,20 @@
+import { Card, Button } from "react-bootstrap";
+
+const ImageCard = ({ image, deleteImage }) => {
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={image.urls.small} />
+      <Card.Body>
+        <Card.Title>
+          {image.title.charAt(0).toUpperCase() + image.title.slice(1)}
+        </Card.Title>
+        <Card.Text>{image.description || image.alt_description}</Card.Text>
+        <Button variant="dark" onClick={() => deleteImage(image.id)}>
+          Delete
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default ImageCard;
