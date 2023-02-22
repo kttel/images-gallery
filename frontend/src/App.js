55c3dde90c7d8d162d0fefe08abcf4b1eb2e2670
elizabeth <63110747+kttel@users.noreply.github.com>
2023-02-22 09:09:47 +0200

    Saving images in the state of App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 const App = () => {
   const [word, setWord] = useState("");
+  const [images, setImages] = useState([]);
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
@@ -15,7 +16,7 @@ const App = () => {
     )
       .then((result) => result.json())
       .then((data) => {
-        console.log(data);
+        setImages([data, ...images]);
       })
       .catch((error) => {
         console.log(error);
