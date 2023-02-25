726b432fb894d3757818bc1d31a03879aa4c4f54
elizabeth <63110747+kttel@users.noreply.github.com>
2023-02-25 18:42:45 +0200

    Implemented flask api with image retrieving

---
+++ api/main.py
@@ -0,0 +1,30 @@
+from flask import Flask, request
+
+from dotenv import load_dotenv
+
+import os
+import requests
+
+UNSPLASH_URL = 'https://api.unsplash.com/photos/random'
+
+load_dotenv(dotenv_path='./.env.local')
+UNSPLASH_KEY = os.environ.get('UNSPLASH_KEY', '')
+
+if not UNSPLASH_KEY:
+  raise EnvironmentError('Please create .env.local file with UNSPLASH_KEY')
+
+app = Flask(__name__)
+
+@app.route('/new-image')
+def new_image():
+  word = request.args.get('query')
+  headers = {
+    'Accept-Version': 'v1',
+    'Authorization': f'Client-ID {UNSPLASH_KEY}',
+  }
+  params = {'query': word}
+  response = requests.get(UNSPLASH_URL, headers=headers, params=params)
+  return response.json()
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=5050, debug=True)
